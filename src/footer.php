9080fbfb2513b8595250562a14bb79c2e4a78968
Marco Pisco <a11531@aejics.org>
2026-06-14 17:46:32 +0100

    chore: remove old files replaced by new architecture
    Removed: login.php, admin.php, mail.php, config.php, configexemplo.json,
    formlist/*.json, src/header.php, src/footer.php, src/main.css, src/.htaccess
    
    All replaced by the new modular architecture under src/lib/, login/, admin/

--- src/footer.php
+++
@@ -1,7 +0,0 @@
-<?php 
-function getLatestCommit() {
-    return substr(file_get_contents(sprintf( '.git/refs/heads/%s', 'main' )),0,8);
-}
-
-echo "<hr><div class='h-100 d-flex align-items-center justify-content-center flex-column'>
-<p class='font-weight-light'><small>(C) " . date('Y') . " AEJICS (coded by Marco Pisco) - <a href='https://github.com/marpisco/formfill/commit/" . getLatestCommit() ."'>Versão " . getLatestCommit() . "</a></i></small></p>" ?>
